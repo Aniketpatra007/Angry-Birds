b4a07618387bf08ab11e81908048362ec3486f1f
Aniket Patra <iamaniketpatra@gmail.com>
2024-10-12 13:13:17 +0530

    Initial commit

---
+++ Assets/Scripts/SlingShotArea.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SlingShotArea : MonoBehaviour
+{
+    [SerializeField] private LayerMask _slingShotAreaMask;
+    public bool isWithinSlingShotArea()
+    {
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        if(Physics2D.OverlapPoint(worldPosition, _slingShotAreaMask))
+        {
+            return true;
+        }
+        else { return false; }
+    }
+}
---
+++ Assets/Scripts/SlingShotHandler.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class SlingShotHandler : MonoBehaviour
+{
+    [Header("Line Renderers")]
+    [SerializeField] private LineRenderer _leftLineRenderer;
+    [SerializeField] private LineRenderer _rightLineRenderer;
+
+    [Header("Transform References")]
+    [SerializeField] private Transform _leftStartPosition;
+    [SerializeField] private Transform _rightStartPosition;
+    [SerializeField] private Transform _centerPosition;
+    [SerializeField] private Transform _idlePosition;
+
+    [Header("SlingShot Stats")]
+    [SerializeField] private float _maxDistance = 3.5f;
+    [SerializeField] private float _shotForce = 9f;
+    [SerializeField] private float _timeBetweenBirdRespawns = 2f;
+
+    [Header("Scripts")]
+    [SerializeField] private SlingShotArea _slingShotArea;
+
+    [Header("Bird")]
+    [SerializeField] private AngieBird _angieBirdPrefab;
+    [SerializeField] private float _angieBirdPositionOffset = 2f;
+
+    private Vector2 _slingShotLinesPosition;
+    private Vector2 _direction;
+    private Vector2 _directionNormalized;
+
+    private bool _clickedWithinArea;
+    private bool _birdOnSlingShot;
+
+    private AngieBird _spawnedAngieBird;
+
+    private void Awake()
+    {
+        _leftLineRenderer.enabled = false;
+        _rightLineRenderer.enabled = false;
+
+        SpawnAngieBird();
+    }
+
+    void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame && _slingShotArea.isWithinSlingShotArea())
+        {
+            _clickedWithinArea = true;
+        }
+
+        if(Mouse.current.leftButton.isPressed && _clickedWithinArea && _birdOnSlingShot)
+        {
+            DrawSlingShot();
+            PositionAndRotateAngieBird();
+        }
+        if(Mouse.current.leftButton.wasReleasedThisFrame && _birdOnSlingShot)
+        {
+            _clickedWithinArea = false;
+
+            _spawnedAngieBird.LaunchBird(_direction, _shotForce);
+
+            _birdOnSlingShot = false;
+
+            SetLines(_centerPosition.position);
+
+            StartCoroutine(SpawnAngieBirdAfterTime());
+        }
+        
+    }
+
+
+    #region SlingShot Methods
+    private void DrawSlingShot()
+    {
+        
+
+        Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        _slingShotLinesPosition = _centerPosition.position + Vector3.ClampMagnitude(touchPosition - _centerPosition.position, _maxDistance);
+
+        SetLines(_slingShotLinesPosition);
+
+        _direction = (Vector2)_centerPosition.position - _slingShotLinesPosition;
+        _directionNormalized = _direction.normalized;
+    }
+
+    private void SetLines(Vector2 position)
+    {
+        if(!_leftLineRenderer.enabled && !_rightLineRenderer.enabled)
+        {
+            _leftLineRenderer.enabled=true;
+            _rightLineRenderer.enabled=true;
+        }
+        _leftLineRenderer.SetPosition(0, position);
+        _leftLineRenderer.SetPosition(1,_leftStartPosition.position);
+
+        _rightLineRenderer.SetPosition(0,position);
+        _rightLineRenderer.SetPosition(1,_rightStartPosition.position);
+    }
+
+    #endregion
+
+    #region Angie Bird Methods
+
+    private void SpawnAngieBird()
+    {
+        SetLines(_idlePosition.position);
+
+        Vector2 dir = (_centerPosition.position - _idlePosition.position).normalized;
+        Vector2 spawnPosition = (Vector2)_idlePosition.position + dir * _angieBirdPositionOffset;
+
+        _spawnedAngieBird =  Instantiate(_angieBirdPrefab, spawnPosition, Quaternion.identity);
+        _spawnedAngieBird.transform.right = dir;
+
+
+        _birdOnSlingShot = true;
+    }
+
+    private void PositionAndRotateAngieBird()
+    {
+        _spawnedAngieBird.transform.position = _slingShotLinesPosition + _directionNormalized *_angieBirdPositionOffset;
+        _spawnedAngieBird.transform.right = _directionNormalized;
+    }
+
+
+    private IEnumerator SpawnAngieBirdAfterTime()
+    {
+        yield return new WaitForSeconds(_timeBetweenBirdRespawns);
+
+        //some code
+        SpawnAngieBird();
+    }
+    #endregion
+}
